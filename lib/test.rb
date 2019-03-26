ebcc71ac1151614568ea5a164af9bbc684249a55
Daniel Schneider <daschne8@gmail.com>
2019-03-26 19:58:52 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -6,8 +6,5 @@ require_relative "../lib/doctor.rb"
 require_relative "../lib/patient.rb"
 require_relative "../lib/appointment.rb"
 
-jay_z = Artist.new("Jay-Z")
-rap = Genre.new("rap")
-ninety_nine_problems = jay_z.new_song("Ninety Nine Problems", rap)
 
 binding.pry
