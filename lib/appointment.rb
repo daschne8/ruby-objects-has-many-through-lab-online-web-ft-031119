993c907bf64809ae6ca5908f2a49b26c452bcdc3
Daniel Schneider <daschne8@gmail.com>
2019-03-26 20:02:43 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,8 +1,11 @@
 class Appointment
   attr_accessor
-  attr_reader
-  @@ = []
-  def initialize()
-
+  attr_reader :date,:patient,:Doctor
+  @@all = []
+  def initialize(date,patient,Doctor)
+    @date,@patient,@doctor = date,patient,Doctor
+  end
+  def self.all
+    @@all
   end
 end
