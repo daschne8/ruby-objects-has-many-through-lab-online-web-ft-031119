ee6f6064af9709fbe7427785da101e953d4923e3
Daniel Schneider <daschne8@gmail.com>
2019-03-26 19:52:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
-  attr_accessor :name,:artist,:genre
-  attr_reader
+  attr_accessor 
+  attr_reader :name,:artist,:genre
   @@all = []
   def initialize(name,artist,genre)
     @name,@artist,@genre = name,artist,genre
