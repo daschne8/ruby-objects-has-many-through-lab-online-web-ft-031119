68887d0a5f763a973b7ec9a38e0f7f772c2c2b39
Daniel Schneider <daschne8@gmail.com>
2019-03-26 20:00:46 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,9 +1,10 @@
 class Doctor
   attr_accessor
   attr_reader
-  @@ = []
-  def initialize()
-
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
   end
   def self.all
     @@all
