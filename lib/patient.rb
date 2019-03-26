fef25ae28234f95865507fd650ef13185c6956d1
Daniel Schneider <daschne8@gmail.com>
2019-03-26 20:09:17 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,4 +12,7 @@ class Patient
   def new_appointment(doctor,date)
     Appointment.new(date,self,doctor)
   end
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
 end
